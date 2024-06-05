095cb8c0a58dd483b4d62f87e7be39394d3e08e6
zetdou <zyziupoz@gmail.com>
2024-06-05 19:24:22 +0200

    TransactionHistory component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import Profile from "./Profile";
 import Statistics from "./Statistics";
 import FriendList from "./FriendList";
+import TransactionHistory from "./TransactionHistory";
 import user from "../data/user.json";
 import data from "../data/data.json";
 import friends from "../data/friends.json";
+import transactions from"../data/transactions.json";
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
       <Statistics title="Upload status" stats={data} />
       <Statistics stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 }
---
+++ src/components/TransactionHistory.jsx
@@ -0,0 +1,41 @@
+import { Component } from "react";
+import PropTypes from "prop-types"
+
+export default class TransactionHistory extends Component {
+    
+    static PropTypes = {
+        items: PropTypes.arrayOf(
+            PropTypes.shape({
+                id: PropTypes.string.isRequired,
+                type: PropTypes.string.isRequired,
+                amount: PropTypes.number.isRequired,
+                currency: PropTypes.string.isRequired,
+            })
+        ).isRequired,
+    };
+
+    render() {
+        const { items } = this.props;
+
+        return (
+            <table className="transaction-history">
+                <thead>
+                    <tr>
+                        <th>Type</th>
+                        <th>Amount</th>
+                        <th>Currency</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {items.map(item => (
+                    <tr key={item.id}>
+                        <td>{item.type}</td>
+                        <td>{item.amount}</td>
+                        <td>{item.currency}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        );
+    }
+}
